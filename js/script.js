fad0ba7e6fb67f02f01337157a4b7a1c009e6fe2
BrillianHakim <130382692+BrillianHakim@users.noreply.github.com>
2024-05-19 12:07:51 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//toggle class active hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+//ketika humburger menu diklik 
+document.querySelector('#hamburger-menu').onclick =()=>{
+    navbarNav.classList.toggle('active');
+}
+
+const searchForm = document.querySelector('.search-form');
+const searchBox =document.querySelector('#search-box');
+// toggle class active search form
+document.querySelector('#search-button').onclick=(e)=>{
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+//klik diluar sidebar menghilangkan navbar
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+
+document.addEventListener('click',function(e){
+    if(!hm.contains(e.target)&&!navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+    if(!sb.contains(e.target)&&!searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
+    }
+});
